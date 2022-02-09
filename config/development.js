7cdd7cf3176ed6939a7200db66705c0ef46e266d
dudek-igor <dudekigor@gmail.com>
2022-02-09 09:22:49 +0100

    Setup dev config

---
+++ config/development.js
@@ -0,0 +1,15 @@
+module.exports = {
+  app: {
+    env: process.env.NODE_ENV || "development",
+    port: process.env.PORT || 3000,
+  },
+  dbConfig: {
+    username: process.env.DATABASE_USER || "root",
+    password: process.env.DATABASE_PASSWORD || "root",
+    database: process.env.DATABASE_DB || "GetnowX",
+    host: process.env.DATABASE_HOST || "db",
+    port: process.env.DATABASE_PORT || 3306,
+    dialect: "mysql",
+    migrationStorageTableName: "migrations",
+  },
+};
