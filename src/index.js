4c650d318cf2116a87ea07fd01c11fa7096dcab8
dudek-igor <dudekigor@gmail.com>
2022-02-09 09:03:08 +0100

    Create entryfile

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * @author  Igor Dudek
+ * @module  AppServer
+ * @info    A index file that contain your app entry point.
+ *          Come on, baby, light my fire 🔥
+ */
+// const AppServer = require("./app");
+// const server = new AppServer();
+// server.listen();
